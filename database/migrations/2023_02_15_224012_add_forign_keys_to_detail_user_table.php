71938a9cdedd171fa3d02c0986f6cdf6e27b7b02
Cashadi <ichascashadi@gmail.com>
2023-02-17 06:13:44 +0700

    add foreign

--- database/migrations/2023_02_15_224012_add_forign_keys_to_detail_user_table.php
+++ database/migrations/2023_02_15_224012_add_forign_keys_to_detail_user_table.php
@@ -15,10 +15,10 @@ class AddForignKeysToDetailUserTable extends Migration
     {
         Schema::table('detail_user', function (Blueprint $table) {
             $table->foreign('user_id','fk_detail_user_to_users')
-            ->reference('id')->on('users')->onDelete('CASCADE')
+            ->references('id')->on('users')->onDelete('CASCADE')
             ->onUpdate('CASCADE');
             $table->foreign('type_user_id','fk_detail_user_to_type_user')
-            ->reference('id')->on('type_user')->onDelete('CASCADE')
+            ->references('id')->on('type_user')->onDelete('CASCADE')
             ->onUpdate('CASCADE');
         });
     }
